a8e7943bcd369fd51f72a3596482ee4b340ece7d
sifiso mdzebele <127992943+zmabel4@users.noreply.github.com>
2023-08-03 13:33:44 +0200

    Add files via upload

--- DWA_5/scripts.js
+++ DWA_5/scripts.js
@@ -7,23 +7,18 @@ form.addEventListener("submit", (event) => {
   const { dividend, divider } = Object.fromEntries(entries);
 
   if (!dividend || !divider) {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-    return;
+    throw new Error("Division not performed. Both values are required in inputs. Try again.");
   }
 
   const dividendValue = parseFloat(dividend);
   const dividerValue = parseFloat(divider);
 
   if (isNaN(dividendValue) || isNaN(dividerValue)) {
-    result.innerText = "Division not performed. Invalid number provided. Try again.";
-    console.error("Invalid number provided:", dividend, divider);
-    return;
+    throw new Error("Division not performed. Invalid number provided. Try again.");
   }
 
   if (dividerValue === 0) {
-    result.innerText = "Division not performed. Cannot divide by zero. Try again.";
-    console.error("Cannot divide by zero:", dividend, divider);
-    return;
+    throw new Error("Division not performed. Cannot divide by zero. Try again.");
   }
 
   const divisionResult = dividendValue / dividerValue;
@@ -34,7 +29,6 @@ form.addEventListener("submit", (event) => {
     result.innerText = `Result: ${divisionResult}`;
   }
 });
-
 
 
 
